746ce1cf60934c0c395afa77254f951c322149fd
yuki.wang <yuki.wang@dianping.com>
2016-04-28 11:28:38 +0800

    个股页面第一页缓存

--- StockNews/StockNews/Class/news/SNStockStatusViewController.m
+++ StockNews/StockNews/Class/news/SNStockStatusViewController.m
@@ -46,10 +46,19 @@
     };
     
     self.tableView.receiveData= ^(SBTableView *tableView ,SBTableData *tableViewData, DataItemResult *result) {
+        NSString *key = [NSString stringWithFormat:@"%@%@", NSStringFromClass(__self.class), __self.channelName];
         if (result.hasError) {
+            if (tableViewData.pageAt == 1 && tableViewData.tableDataResult.dataList.count == 0) {
+                DataItemResult *cacheResult = [[SBAppCoreInfo getCacheDB] getResultValue:STORE_CACHE_TABLEDATA dataKey:key];
+                [tableViewData.tableDataResult appendItems:cacheResult];
+            }
             return;
         }
         
+        if (tableViewData.pageAt == 1) {
+            // 对数据第一页做缓存
+            [[SBAppCoreInfo getCacheDB] setResultValue:STORE_CACHE_TABLEDATA dataKey:key data:result];
+        }
         [tableViewData.tableDataResult appendItems:result];
     };
     
