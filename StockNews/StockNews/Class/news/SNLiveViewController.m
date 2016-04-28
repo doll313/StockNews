615c9e6f5230efcac0bb9dc29c350d742fb4dc26
yuki.wang <yuki.wang@dianping.com>
2016-04-28 11:24:44 +0800

    直播页面增加第一页缓存

--- StockNews/StockNews/Class/news/SNLiveViewController.m
+++ StockNews/StockNews/Class/news/SNLiveViewController.m
@@ -49,12 +49,6 @@
             minid = [tableViewData.tableDataResult.resultInfo getString:@"MinID"];
         }
         
-        // 类似直播这种有分割时间显示的
-        if (tableViewData.pageAt == 1 && __self.dateArray.count > 0) {
-            // 重新刷新的时候，清除时间数组
-            [__self.dateArray removeAllObjects];
-        }
-        
         if (__self.column.length == 0 && __self.channelName.length > 0) {
             return [SNDataProcess snget_important_news_list:minid encode:__self.channelName delegate:tableViewData];
         }
@@ -63,50 +57,26 @@
     };
     
     self.tableView.receiveData= ^(SBTableView *tableView ,SBTableData *tableViewData, DataItemResult *result) {
+        NSString *key = [NSString stringWithFormat:@"%@%@%@", NSStringFromClass(__self.class), __self.channelName, __self.column];
         if (result.hasError) {
+            if (tableViewData.pageAt == 1 && tableViewData.tableDataResult.dataList.count == 0) {
+                DataItemResult *cacheResult = [[SBAppCoreInfo getCacheDB] getResultValue:STORE_CACHE_TABLEDATA dataKey:key];
+                if (__self.dateArray.count > 0) {
+                    [__self.dateArray removeAllObjects];
+                }
+                [__self appendData:tableViewData result:cacheResult];
+            }
             return;
         }
         
-        tableViewData.tableDataResult.maxCount = result.maxCount;
-        tableViewData.tableDataResult.statusCode = result.statusCode;
-        tableViewData.tableDataResult.message = result.message;
-        tableViewData.tableDataResult.itemUniqueKeyName = result.itemUniqueKeyName;
-        
-        NSDictionary *itemInfo = result.resultInfo.dictData;
-        for(NSString *key in [itemInfo allKeys]){
-            NSString *value = (NSString *)itemInfo[key];
-            [tableViewData.tableDataResult.resultInfo setString:value forKey:key];
-        }
-        
-        SBTableData *lastTableData = tableViewData;
-        for (DataItemDetail *detail in result.dataList) {
-            NSString *date = [detail getString:SN_OTHER_LIST_TIME];
-            date = [SBTimeManager sb_formatStr:date preFormat:@"yyyy-MM-dd HH:mm:ss" newFormat:@"yyyy年MM月dd日"];
-            if (![__self.dateArray containsObject:date]) {
-                [__self.dateArray addObject:date];
-                if (__self.dateArray.count > 1) {
-                    // 创建新的section
-                    SBTableData *sectionData = [[SBTableData alloc] init];
-                    sectionData.hasHeaderView = YES;
-                    sectionData.mDataCellClass = [SNLiveCell class];
-                    sectionData.hasFinishCell = YES;
-                    sectionData.httpStatus = SBTableDataStatusFinished;
-                    [__self.tableView addSectionWithData:sectionData];
-                    [sectionData.tableDataResult addItem:detail];
-                    lastTableData = sectionData;
-                    
-                    sectionData.tableDataResult.resultInfo = tableViewData.tableDataResult.resultInfo;
-                    sectionData.tableDataResult.maxCount = result.maxCount;
-                    sectionData.pageSize = tableViewData.pageSize;
-                    sectionData.pageAt = tableViewData.pageAt;
-                    continue;
-                }
+        if (tableViewData.pageAt == 1) {
+            // 对数据第一页做缓存
+            [[SBAppCoreInfo getCacheDB] setResultValue:STORE_CACHE_TABLEDATA dataKey:key data:result];
+            if (__self.dateArray.count > 0) {
+                [__self.dateArray removeAllObjects];
             }
-            [lastTableData.tableDataResult addItem:detail];
         }
-        if (lastTableData != tableViewData) {
-            [lastTableData loadData];
-        }
+        [__self appendData:tableViewData result:result];
     };
     
     self.tableView.headerForSection = ^(SBTableView *tableView, NSInteger section) {
@@ -148,6 +118,49 @@
     bottomLine.frame = CGRectMake(0, 19, self.view.width, 1);
     [headerView addSubview:bottomLine];
     return headerView;
+}
+
+- (void)appendData:(SBTableData *)tableViewData result:(DataItemResult *)result {
+    tableViewData.tableDataResult.maxCount = result.maxCount;
+    tableViewData.tableDataResult.statusCode = result.statusCode;
+    tableViewData.tableDataResult.message = result.message;
+    tableViewData.tableDataResult.itemUniqueKeyName = result.itemUniqueKeyName;
+    
+    NSDictionary *itemInfo = result.resultInfo.dictData;
+    for(NSString *key in [itemInfo allKeys]){
+        NSString *value = (NSString *)itemInfo[key];
+        [tableViewData.tableDataResult.resultInfo setString:value forKey:key];
+    }
+    
+    SBTableData *lastTableData = tableViewData;
+    for (DataItemDetail *detail in result.dataList) {
+        NSString *date = [detail getString:SN_OTHER_LIST_TIME];
+        date = [SBTimeManager sb_formatStr:date preFormat:@"yyyy-MM-dd HH:mm:ss" newFormat:@"yyyy年MM月dd日"];
+        if (![self.dateArray containsObject:date]) {
+            [self.dateArray addObject:date];
+            if (self.dateArray.count > 1) {
+                // 创建新的section
+                SBTableData *sectionData = [[SBTableData alloc] init];
+                sectionData.hasHeaderView = YES;
+                sectionData.mDataCellClass = [SNLiveCell class];
+                sectionData.hasFinishCell = YES;
+                sectionData.httpStatus = SBTableDataStatusFinished;
+                [self.tableView addSectionWithData:sectionData];
+                [sectionData.tableDataResult addItem:detail];
+                lastTableData = sectionData;
+                
+                sectionData.tableDataResult.resultInfo = tableViewData.tableDataResult.resultInfo;
+                sectionData.tableDataResult.maxCount = result.maxCount;
+                sectionData.pageSize = tableViewData.pageSize;
+                sectionData.pageAt = tableViewData.pageAt;
+                continue;
+            }
+        }
+        [lastTableData.tableDataResult addItem:detail];
+    }
+    if (lastTableData != tableViewData) {
+        [lastTableData loadData];
+    }
 }
 
 @end
